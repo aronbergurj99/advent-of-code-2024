f5c3bb549d6d9140d0d71d340f1c11468abcbdce
Aron Bergur Johannsson <aronbergurj@outlook.com>
2024-12-09 10:12:45 +0000

    day08 part 2

--- src/main/scala/day08/Day08.scala
+++ src/main/scala/day08/Day08.scala
@@ -4,6 +4,7 @@ import zio.{*, given}
 import zio.Console.*
 import utils.Utils.*
 import scala.math.*
+import scala.util.Try
 
 type Pos = (Int, Int)
 type Grid = Vector[Vector[Char]]
@@ -28,17 +29,17 @@ def findAntennas(grid: Grid) =
     }
   }
 
-def findAntinodes(p1: Pos, p2: Pos) =
+def findAntinodes(p1: Pos, p2: Pos, n: Int) =
   val dx = p2._2 - p1._2
   val dy = p1._1 - p2._1
 
-  (p1._1 - dy * 2, p1._2 + dx * 2)
+  (p1._1 - dy * n, p1._2 + dx * n)
 
 
 def part1(grid: Grid) =
   for {
     antennas  <- findAntennas(grid)
-    antiNodes <- ZIO.foreach(antennas.map(findAntinodes).toList.distinct) { antiNode =>
+    antiNodes <- ZIO.foreach(antennas.map(findAntinodes(_, _, 2)).toList.distinct) { antiNode =>
       ZIO.attempt {
         grid(antiNode.head)(antiNode.last)
         true
@@ -46,12 +47,30 @@ def part1(grid: Grid) =
     }.map(_.count(identity))
   } yield (antiNodes)
 
+
+
+def part2(grid: Grid) =
+  for {
+    antennas  <- findAntennas(grid)
+    antiNodes <- ZIO.foreach(antennas) { antenna => 
+      def loop(n: Int, acc: List[Pos]): List[Pos] =
+        val node = findAntinodes(antenna.head, antenna.last, n)
+
+        Try(grid(node.head)(node.last)).toOption match
+          case None => acc
+          case Some(value) => loop(n + 1, node :: acc)
+
+      ZIO.succeed(loop(1, List.empty))
+    }
+  } yield (antiNodes.flatMap(identity).toList.distinct.length)
+
 object App extends ZIOAppDefault {
   def run = for {
     input  <- loadResource("day08.in")
     grid <- parse(input)
     ans1   <- part1(grid)
     _      <- printLine(ans1)
-
+    ans2   <- part2(grid)
+    _      <- printLine(ans2)
   } yield ()
 }
